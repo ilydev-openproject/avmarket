00a8f9e2463b5f442f5bfe97afa7f59687e949b1
Ilyas Maazib <ilyasmaazib9@gmail.com>
2025-02-13 06:40:01 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -425,29 +425,28 @@
         <section class="slider-area tpslider-delay">
             <div class="swiper-container slider-active">
                 <div class="swiper-wrapper">
-                    @foreach ($hero as $hero)
                     <div class="swiper-slide ">
-                        <div class="tpslider pt-90 pb-0 grey-bg" data-background="orfarm/assets/img/slider/shape-bg.jpg">
+                        <div class="tpslider pt-90 pb-0 grey-bg" data-background="assets/img/slider/shape-bg.jpg">
                             <div class="container">
                                 <div class="row align-items-center">
                                     <div class="col-xxl-5 col-lg-6 col-md-6 col-12 col-sm-6">
                                         <div class="tpslider__content pt-20">
-                                            <span class="tpslider__sub-title mb-35">{{$hero->subheader}}</span>
-                                            <h2 class="tpslider__title mb-30">{{ $hero->header }}.</h2>
-                                            <p>{!! $hero->paragraph !!}</p>
+                                            <span class="tpslider__sub-title mb-35">Top Seller In The Week</span>
+                                            <h2 class="tpslider__title mb-30">Choose Your Healthy Lifestyle.</h2>
+                                            <p>Presentation matters. Our fresh Vietnamese vegetable rolls <br> look good and taste even better</p>
                                             <div class="tpslider__btn">
-                                                <a class="tp-btn" href="shop-2.html">Get Now</a>
+                                                <a class="tp-btn" href="shop-2.html">Shop Now</a>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-xxl-7 col-lg-6 col-md-6 col-12 col-sm-6">
                                         <div class="tpslider__thumb p-relative pt-15">
-                                            <img class="tpslider__thumb-img" src="{{ $hero->getFirstMediaUrl('foto_hero') }}" alt="slider-bg">
+                                            <img class="tpslider__thumb-img" src="assets/img/slider/slider-bg-1.png" alt="slider-bg">
                                             <div class="tpslider__shape d-none d-md-block">
-                                                <img class="tpslider__shape-one" src="orfarm/assets/img/slider/slider-shape-1.png" alt="shape">
-                                                <img class="tpslider__shape-two" src="orfarm/assets/img/slider/slider-shape-2.png" alt="shape">
-                                                <img class="tpslider__shape-three" src="orfarm/assets/img/slider/slider-shape-3.png" alt="shape">
-                                                <img class="tpslider__shape-four" src="orfarm/assets/img/slider/slider-shape-4.png" alt="shape">
+                                                <img class="tpslider__shape-one" src="assets/img/slider/slider-shape-1.png" alt="shape">
+                                                <img class="tpslider__shape-two" src="assets/img/slider/slider-shape-2.png" alt="shape">
+                                                <img class="tpslider__shape-three" src="assets/img/slider/slider-shape-3.png" alt="shape">
+                                                <img class="tpslider__shape-four" src="assets/img/slider/slider-shape-4.png" alt="shape">
                                             </div>
                                         </div>
                                     </div>
@@ -455,7 +454,64 @@
                             </div>
                         </div>
                     </div>
-                    @endforeach
+                    <div class="swiper-slide">
+                        <div class="tpslider pt-90 pb-0 grey-bg" data-background="assets/img/slider/shape-bg.jpg">
+                            <div class="container">
+                                <div class="row align-items-center">
+                                    <div class="col-xxl-5 col-lg-6 col-md-6 col-sm-6">
+                                        <div class="tpslider__content pt-20">
+                                            <span class="tpslider__sub-title mb-35">Top Seller In The Week</span>
+                                            <h2 class="tpslider__title mb-30">Fresh Bread <br> Oatmeal Crumble. </h2>
+                                            <p>Presentation matters. Our fresh Vietnamese vegetable rolls <br> look good and taste even better</p>
+                                            <div class="tpslider__btn">
+                                                <a class="tp-btn" href="shop-2.html">Shop Now</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-7 col-lg-6  col-md-6 col-sm-6">
+                                        <div class="tpslider__thumb p-relative">
+                                            <img class="tpslider__thumb-img" src="assets/img/slider/slider-bg-2.png" alt="slider-bg">
+                                            <div class="tpslider__shape d-none d-md-block">
+                                                <img class="tpslider__shape-one" src="assets/img/slider/slider-shape-1.png" alt="shape">
+                                                <img class="tpslider__shape-two" src="assets/img/slider/slider-shape-2.png" alt="shape">
+                                                <img class="tpslider__shape-three" src="assets/img/slider/slider-shape-3.png" alt="shape">
+                                                <img class="tpslider__shape-four" src="assets/img/slider/slider-shape-4.png" alt="shape">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="tpslider pt-90 pb-0 grey-bg" data-background="assets/img/slider/shape-bg.jpg">
+                            <div class="container">
+                                <div class="row align-items-center">
+                                    <div class="col-xxl-5 col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                        <div class="tpslider__content pt-20">
+                                            <span class="tpslider__sub-title mb-35">Top Seller In The Week</span>
+                                            <h2 class="tpslider__title mb-30">The Best <br> Health Fresh.</h2>
+                                            <p>Presentation matters. Our fresh Vietnamese vegetable rolls <br> look good and taste even better</p>
+                                            <div class="tpslider__btn">
+                                                <a class="tp-btn" href="shop-2.html">Shop Now</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-7 col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                        <div class="tpslider__thumb p-relative">
+                                            <img class="tpslider__thumb-img" src="assets/img/slider/slider-bg-3.png" alt="slider-bg">
+                                            <div class="tpslider__shape d-none d-md-block">
+                                                <img class="tpslider__shape-one" src="assets/img/slider/slider-shape-1.png" alt="shape">
+                                                <img class="tpslider__shape-two" src="assets/img/slider/slider-shape-2.png" alt="shape">
+                                                <img class="tpslider__shape-three" src="assets/img/slider/slider-shape-3.png" alt="shape">
+                                                <img class="tpslider__shape-four" src="assets/img/slider/slider-shape-4.png" alt="shape">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="tpslider__arrow d-none  d-xxl-block">
                     <button class="tpsliderarrow tpslider__arrow-prv"><i class="icon-chevron-left"></i></button>
