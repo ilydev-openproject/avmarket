a7b172eeafaffb10f96b7509b8024f64099d538a
Ilyas Maazib <ilyasmaazib9@gmail.com>
2025-02-13 06:54:17 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -427,7 +427,7 @@
                 <div class="swiper-wrapper">
                     @foreach ($hero as $hero)
                     <div class="swiper-slide ">
-                        <div class="tpslider pt-90 pb-0 grey-bg" data-background="orfarm/assets/img/slider/shape-bg.jpg">
+                        <div class="tpslider pt-90 pb-0 grey-bg" data-background="orfarm/assets/img/slider/shape-bg.jpg" style="background-size: cover; background-repeat: no-repeat;">
                             <div class="container">
                                 <div class="row align-items-center">
                                     <div class="col-xxl-5 col-lg-6 col-md-6 col-12 col-sm-6">
